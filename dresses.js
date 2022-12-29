40d796f577eb8084669a9be13d88b99d1bb79aa9
chrisjoness2 <91157652+chrisjoness2@users.noreply.github.com>
2022-12-29 01:34:00 -0600

    Add files via upload

---
+++ dresses.js
@@ -0,0 +1,138 @@
+function openNav() {
+
+    	if  (screen.width >= 700) {
+        document.getElementById("myNav").style.width = "20%";
+      }
+
+      if (screen.width <= 699) {
+        document.getElementById("myNav").style.width = "50%";
+      }
+       else {
+    	document.getElementById("myNav").style.height = "100%";
+    	}
+    }
+
+
+
+
+function closeNav() {
+  if (screen.width >= 700) {
+        document.getElementById("myNav").style.width = "0%";
+  }
+  if (screen.width <= 699) {
+    document.getElementById("myNav").style.width = "0%";
+  }
+  else {
+    	document.getElementById("myNav").style.height = "100%";
+    	}
+    }
+
+// Image 1
+function setNewImage() {
+  document.getElementById("all-items-listed-img1").src = "dresses/dress1hover.jpg";
+}
+
+
+function setOldImage() {
+  document.getElementById("all-items-listed-img1").src = "dresses/dress1.jpg";
+}
+
+
+if (screen.width <= 699) {
+  document.getElementById("all-items-listed-img1").src= "dresses/dress1hover.jpg";
+}
+//Image2
+function setNewImage2() {
+  document.getElementById("all-items-listed-img2").src = "dresses/dress2hover.jpg";
+}
+
+
+function setOldImage2() {
+  document.getElementById("all-items-listed-img2").src = "dresses/dress2.jpg";
+}
+
+
+if (screen.width <= 699) {
+  document.getElementById("all-items-listed-img2").src= "dresses/dress2hover.jpg";
+}
+//Image 3
+function setNewImage3() {
+  document.getElementById("all-items-listed-img3").src = "dresses/dress3hover.jpg";
+}
+
+
+function setOldImage3() {
+  document.getElementById("all-items-listed-img3").src = "dresses/dress3.jpg";
+}
+
+if (screen.width <= 699) {
+  document.getElementById("all-items-listed-img3").src= "dresses/dress3hover.jpg";
+}
+
+//Image4
+function setNewImage4() {
+  document.getElementById("all-items-listed-img4").src = "dresses/dress4hover.jpg";
+}
+
+
+function setOldImage4() {
+  document.getElementById("all-items-listed-img4").src = "dresses/dress4.jpg";
+}
+
+if (screen.width <= 699) {
+  document.getElementById("all-items-listed-img4").src= "dresses/dress4hover.jpg";
+}
+//Image5
+function setNewImage5() {
+  document.getElementById("all-items-listed-img5").src = "dresses/dress5hover.jpg";
+}
+
+
+function setOldImage5() {
+  document.getElementById("all-items-listed-img5").src = "dresses/dress5.jpg";
+}
+
+if (screen.width <= 699) {
+  document.getElementById("all-items-listed-img5").src= "dresses/dress5hover.jpg";
+}
+
+//Image6
+function setNewImage6() {
+  document.getElementById("all-items-listed-img6").src = "dresses/dress6hover.jpg";
+}
+
+
+function setOldImage6() {
+  document.getElementById("all-items-listed-img6").src = "dresses/dress6.jpg";
+}
+
+if (screen.width <= 699) {
+  document.getElementById("all-items-listed-img6").src= "dresses/dress6hover.jpg";
+}
+//Image7
+function setNewImage7() {
+  document.getElementById("all-items-listed-img7").src = "dresses/dress7hover.jpg";
+}
+
+
+function setOldImage7() {
+  document.getElementById("all-items-listed-img7").src = "dresses/dress7.jpg";
+}
+
+if (screen.width <= 699) {
+  document.getElementById("all-items-listed-img7").src= "dresses/dress7hover.jpg";
+}
+
+//Image8
+function setNewImage8() {
+  document.getElementById("all-items-listed-img8").src = "dresses/dress8hover.jpg";
+}
+
+
+function setOldImage8() {
+  document.getElementById("all-items-listed-img8").src = "dresses/dress8.jpg";
+}
+
+if (screen.width <= 699) {
+  document.getElementById("all-items-listed-img8").src= "dresses/dress8hover.jpg";
+}
---
+++ index.js
@@ -0,0 +1,143 @@
+
+function openNav() {
+
+    	if  (screen.width >= 700) {
+        document.getElementById("myNav").style.width = "20%";
+      }
+
+      if (screen.width <= 699) {
+        document.getElementById("myNav").style.width = "50%";
+      }
+       else {
+    	document.getElementById("myNav").style.height = "100%";
+    	}
+    }
+
+
+
+
+function closeNav() {
+  if (screen.width >= 700) {
+        document.getElementById("myNav").style.width = "0%";
+  }
+  if (screen.width <= 699) {
+    document.getElementById("myNav").style.width = "0%";
+  }
+  else {
+    	document.getElementById("myNav").style.height = "100%";
+    	}
+    }
+
+
+// Image 1
+function setNewImage() {
+  document.getElementById("all-items-listed-img1").src = "tops/top1hover.jpg";
+}
+
+
+function setOldImage() {
+  document.getElementById("all-items-listed-img1").src = "tops/top1.jpeg";
+}
+
+if (screen.width <= 699) {
+  document.getElementById("all-items-listed-img1").src= "tops/top1hover.jpg";
+}
+
+
+
+//Image2
+function setNewImage2() {
+  document.getElementById("all-items-listed-img2").src = "tops/top2hover.jpg";
+}
+
+
+function setOldImage2() {
+  document.getElementById("all-items-listed-img2").src = "tops/top2.jpeg";
+}
+
+if (screen.width <= 699) {
+  document.getElementById("all-items-listed-img2").src= "tops/top2hover.jpg";
+}
+
+
+//Image 3
+function setNewImage3() {
+  document.getElementById("all-items-listed-img3").src = "tops/top3hover.jpg";
+}
+
+
+function setOldImage3() {
+  document.getElementById("all-items-listed-img3").src = "tops/top3.jpeg";
+}
+
+if (screen.width <= 699) {
+  document.getElementById("all-items-listed-img3").src= "tops/top3hover.jpg";
+}
+
+//Image4
+function setNewImage4() {
+  document.getElementById("all-items-listed-img4").src = "tops/top4hover.jpg";
+}
+
+
+function setOldImage4() {
+  document.getElementById("all-items-listed-img4").src = "tops/top4.jpeg";
+}
+
+if (screen.width <= 699) {
+  document.getElementById("all-items-listed-img4").src= "tops/top4hover.jpg";
+}
+//Image5
+function setNewImage5() {
+  document.getElementById("all-items-listed-img5").src = "tops/top5hover.jpg";
+}
+
+
+function setOldImage5() {
+  document.getElementById("all-items-listed-img5").src = "tops/top5.jpeg";
+}
+
+if (screen.width <= 699) {
+  document.getElementById("all-items-listed-img5").src= "tops/top5hover.jpg";
+}
+//Image6
+function setNewImage6() {
+  document.getElementById("all-items-listed-img6").src = "tops/top6hover.jpg";
+}
+
+
+function setOldImage6() {
+  document.getElementById("all-items-listed-img6").src = "tops/top6.jpeg";
+}
+
+if (screen.width <= 699) {
+  document.getElementById("all-items-listed-img6").src= "tops/top6hover.jpg";
+}
+//Image7
+function setNewImage7() {
+  document.getElementById("all-items-listed-img7").src = "tops/top7hover.jpg";
+}
+
+
+function setOldImage7() {
+  document.getElementById("all-items-listed-img7").src = "tops/top7.jpeg";
+}
+
+if (screen.width <= 699) {
+  document.getElementById("all-items-listed-img7").src= "tops/top7hover.jpg";
+}
+
+//Image8
+function setNewImage8() {
+  document.getElementById("all-items-listed-img8").src = "tops/top8hover.jpg";
+}
+
+
+function setOldImage8() {
+  document.getElementById("all-items-listed-img8").src = "tops/top8.jpeg";
+}
+
+
+if (screen.width <= 699) {
+  document.getElementById("all-items-listed-img8").src= "tops/top8hover.jpg";
+}
